850db858e84b16ca934789f2ef10e302f451f50e
Juan David Villada <juan.villadar@upb.edu.co>
2025-08-21 11:35:36 -0500

    Se hizo ejercicio de IMC

--- SRC/Ejercicio1.py
+++ SRC/Ejercicio1.py
@@ -16,3 +16,17 @@ IMC = peso/altura**2
 #Mostrar IMC
 print("Tu IMC =", IMC)
 
+if IMC >= 18.5 and IMC <= 24.9:
+    print("Normal")
+else:
+    if IMC >= 25 and IMC <= 29.9:
+        print("Sobrepeso")
+    else:
+        if IMC >= 30 and IMC <= 34.9:
+            print("Sobrepeso I")
+        else:
+            if IMC >= 35 and IMC <= 39.9:
+                print("Sobrepeso II")
+            else:
+                if IMC >= 40:
+                    print("Sobrepeso II")
