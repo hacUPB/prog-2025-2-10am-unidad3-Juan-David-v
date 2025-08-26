1a078a60da66fcda69893835d5fff827cef68c5f
Juan David Villada <juan.villadar@upb.edu.co>
2025-08-26 11:26:30 -0500

    ejercicio condicionales

--- SRC/Ejercicio3.py
+++ SRC/Ejercicio3.py
@@ -6,4 +6,26 @@ numero = int(input("Ingresar el numero; "))
 residuo=( numero % 3)
 
 if residuo == 0:
-    print(numero, "es divisible en 3")
+    print(f"{numero}, es divisible en 3")
+
+
+
+#Condicional Doble: 
+print("=== MENÚ PRINCIPAL ===")
+print("1. Ver datos de sensores")
+print("2. Configurar parámetros")
+print("3. Salir")
+
+opcion = int(input("Selecciona una opción: "))
+
+match opcion:
+    case 1:
+        print("Mostrando datos de sensores...")
+    case 2:
+        print("Entrando a configuración...")
+    case 3:
+        print("Saliendo del programa...")
+    case _:
+        print("Opción inválida.")
+
+
